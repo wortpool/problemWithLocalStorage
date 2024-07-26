6489f8746b814cb7790832171feefc376c464c63
Igor Savula <47925815+wortpool@users.noreply.github.com>
2024-07-26 15:53:19 +0300

    Delete src directory

--- src/api/prices.js
+++
@@ -1,4 +0,0 @@
-import { useQuery } from "react-query";
-import { getPrice } from "../services/pricesApi";
-
-export const useGetPrice = (coin) => useQuery({queryKey: ['price', coin], queryFn: () => getPrice(coin), refetchInterval: 2000})
--- src/services/pricesApi.js
+++
@@ -1,5 +0,0 @@
-import axiosIns from "./axiosIns"
-
-export const getPrice = async (coin) => {
-    return await axiosIns.get(`/v5/market/tickers?category=inverse&symbol=${coin}`)
-}
